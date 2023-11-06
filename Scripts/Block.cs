ef9fef260bd4c9b3e03495872600ecc04cb5dadc
Remonone <50263589+Remonone@users.noreply.github.com>
2023-11-06 13:31:33 +0200

    Building a mesh for block.

---
+++ Scripts/Block.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace {
+    public class Block : MonoBehaviour {
+        
+        [SerializeField] private BlockSide side;
+        [SerializeField] private Vector3 _offset;
+        
+        [Serializable]
+        public enum BlockSide {
+            BOTTOM,
+            TOP,
+            LEFT,
+            RIGHT,
+            FRONT,
+            BACK
+        };
+        
+        private void Start() {
+            MeshFilter mf = gameObject.AddComponent<MeshFilter>();
+            MeshRenderer mr = gameObject.AddComponent<MeshRenderer>();
+
+            Quad q = new Quad();
+            mf.mesh = q.BuildMesh(side, _offset);
+        }
+    }
+}
--- Scripts/Quad.cs
+++ Scripts/Quad.cs
@@ -1,46 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using DefaultNamespace;
 using UnityEngine;
 
-public class Quad : MonoBehaviour {
+public class Quad {
+
+    private static Dictionary<string, Vector2> _uvs = new() {
+        {"uv00", new Vector2(0, 0)},
+        {"uv01", new Vector2(0, 1)},
+        {"uv10", new Vector2(1, 0)},
+        {"uv11", new Vector2(1, 1)}
+    };
+
+    private static Dictionary<string, Vector3> _vertices = new() {
+        { "p0", new Vector3(-0.5f, -0.5f, 0.5f) },
+        { "p1", new Vector3(0.5f, -0.5f, 0.5f) },
+        { "p2", new Vector3(0.5f, -0.5f, -0.5f) },
+        { "p3", new Vector3(-0.5f, -0.5f, -0.5f) },
+        { "p4", new Vector3(-0.5f, 0.5f, 0.5f) },
+        { "p5", new Vector3(0.5f, 0.5f, 0.5f) },
+        { "p6", new Vector3(0.5f, 0.5f, -0.5f) },
+        { "p7", new Vector3(-0.5f, 0.5f, -0.5f) }
+    };
+
+    private Dictionary<Block.BlockSide, (Vector3[] vertices, Vector3[] normals, Vector2[] uvs, int[] triangles)> _sides = new() {
+            {
+                Block.BlockSide.FRONT, 
+                (new []{_vertices["p4"], _vertices["p5"], _vertices["p1"], _vertices["p0"]}, 
+                new [] { Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward }, 
+                new []{_uvs["uv11"], _uvs["uv01"], _uvs["uv00"], _uvs["uv00"]},
+                new []{3, 1, 0, 3, 2, 1})
+            },
+            {
+                Block.BlockSide.BACK,
+                (new []{_vertices["p6"], _vertices["p7"], _vertices["p3"], _vertices["p2"]}, 
+                new []{Vector3.back, Vector3.back, Vector3.back, Vector3.back},
+                new []{_uvs["uv11"], _uvs["uv01"], _uvs["uv00"], _uvs["uv00"]},
+                new []{3, 1, 0, 3, 2, 1})
+            },
+            {
+                Block.BlockSide.LEFT,
+                (new []{_vertices["p5"], _vertices["p6"], _vertices["p2"], _vertices["p1"]},
+                new []{Vector3.left, Vector3.left, Vector3.left, Vector3.left},
+                new [] {_uvs["uv11"], _uvs["uv01"], _uvs["uv00"], _uvs["uv00"]},
+                new [] {3, 1, 0, 3, 2, 1})
+            },
+            {
+                Block.BlockSide.RIGHT,
+                (new []{_vertices["p7"], _vertices["p4"], _vertices["p0"], _vertices["p3"]},
+                new []{Vector3.right, Vector3.right, Vector3.right, Vector3.right},
+                new [] {_uvs["uv11"], _uvs["uv01"], _uvs["uv00"], _uvs["uv00"]},
+                new [] {3, 1, 0, 3, 2, 1})
+            },
+            {
+                Block.BlockSide.TOP,
+                (new []{_vertices["p7"], _vertices["p6"], _vertices["p5"], _vertices["p4"]},
+                new []{Vector3.up, Vector3.up, Vector3.up, Vector3.up},
+                new [] {_uvs["uv11"], _uvs["uv01"], _uvs["uv00"], _uvs["uv00"]},
+                new [] {3, 1, 0, 3, 2, 1})
+            },
+            {
+                Block.BlockSide.BOTTOM,
+                (new []{_vertices["p0"], _vertices["p1"], _vertices["p2"], _vertices["p3"]},
+                new []{Vector3.down, Vector3.down, Vector3.down, Vector3.down},
+                new [] {_uvs["uv11"], _uvs["uv01"], _uvs["uv00"], _uvs["uv00"]},
+                new [] {3, 1, 0, 3, 2, 1})
+            }
+        };
     
-    private void Start() {
-        Mesh mesh;
-        var mf = this.gameObject.AddComponent<MeshFilter>();
-        var mr = this.gameObject.AddComponent<MeshRenderer>();
-
-        mesh = new Mesh();
-        mesh.name = "Custom Quad";
-        var vertices = new Vector3[4];
-        var normals = new Vector3[4];
-        var uvs = new Vector2[4];
-        var triangles = new int[6];
-
-        Vector2 uv00 = new Vector2( 0, 0 );
-        Vector2 uv01 = new Vector2(0, 1);
-        Vector2 uv10 = new Vector2(1, 0);
-        Vector2 uv11 = new Vector2(1, 1);
-
-        Vector3 p0 = new Vector3(-0.5f, -0.5f, 0.5f);
-        Vector3 p1 = new Vector3(0.5f, -0.5f, 0.5f);
-        Vector3 p2 = new Vector3(0.5f, -0.5f, -0.5f);
-        Vector3 p3 = new Vector3(-0.5f, -0.5f, -0.5f);
-        Vector3 p4 = new Vector3(0.5f, 0.5f, 0.5f);
-        Vector3 p5 = new Vector3(0.5f, 0.5f, -0.5f);
-        Vector3 p6 = new Vector3(0.5f, 0.5f, -0.5f);
-        Vector3 p7 = new Vector3(-0.5f, 0.5f, -0.5f);
-
-        vertices = new Vector3[] { p4, p5, p1, p0 };
-        normals = new Vector3[] { Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward };
-        uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
-        triangles = new int[] { 3, 1, 0, 3, 2, 1 };
-
-        mesh.vertices = vertices;
-        mesh.normals = normals;
-        mesh.uv = uvs;
-        mesh.triangles = triangles;
+    public Mesh BuildMesh(Block.BlockSide side, Vector3 offset) {
+        Mesh mesh = new Mesh { name = "Custom Quad" };
+       
+        mesh.vertices = _sides[side].vertices.Select(vertex => vertex + offset).ToArray();
+        mesh.normals = _sides[side].normals;
+        mesh.uv = _sides[side].uvs;
+        mesh.triangles = _sides[side].triangles;
 
         mesh.RecalculateBounds();
+        
+        return mesh;
     }
 
 }
